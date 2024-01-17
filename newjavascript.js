9f94cd5c8a4f2a280c6750e1352f0666947ab64e
Jesus Miralles <97607150+JesusMiralles@users.noreply.github.com>
2024-01-17 20:03:39 +0100

    Update newjavascript.js

--- newjavascript.js
+++ newjavascript.js
@@ -1,5 +1,11 @@
 
-function toggleMenu(menu) {
-    menu.classList.toggle('open');
-  }
-       
+// js para manejar el menú hamburguesa
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerIcon = document.getElementById('burger-icon');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  burgerIcon.addEventListener('click', function () {
+    this.classList.toggle('open');
+    mobileMenu.classList.toggle('open');
+  });
+});    
